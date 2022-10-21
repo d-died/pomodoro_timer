a693d041cd5e4cde5885d6c353400d420bdbaacc
Derek Diedrich <dsumpmann@gmail.com>
2022-10-21 09:34:29 -0700

    First commit with lots of updates. Just added manual start functionality

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
+import tkinter
 from tkinter import *
 import math
+from functools import partial
+
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+from tkinter import ttk
+
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+
 reps = 0
 timer = None
+color_options = [
+    {"default": {"bg": "#f7f5dd", "work": "#9bdeac", "short": "#e2979c", "long": "#e7305b"}},
+    {"dark_mode": {"bg": "#3f3939", "work": "#bcbcbc", "short": "#74666d", "long": "#a4b885"}},
+    {"d_faves": {"bg": "#601ba2", "work": "#35ea4D", "short": "#9fd329", "long": "#d9d2e9"}},
+    {"desert": {"bg": "#d8a300", "work": "#ef8552", "short": "#6aa84f", "long": "#9fc5e8"}}
+]
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+default = color_options[0]["default"]
+dark_mode = color_options[1]["dark_mode"]
+d_faves = color_options[2]["d_faves"]
+desert = color_options[3]["desert"]
+selected_theme = {}
+color_mode = [default, dark_mode, d_faves, desert]
+
+short_timer_settings = {"work": 0.5, "short": 0.5, "long": 0.5}
+long_timer_settings = {"work": 0.5, "short": 0.5, "long": 0.5}
+
+# ---------------------------- CHANGE COLOR THEME ------------------------------- #
+
+index = 0
+def change_colors():
+    global index, selected_theme
+    if index <= 2:
+        index += 1
+    else:
+        index = 0
+    selected_theme = color_mode[index]
+    window.config(bg=selected_theme["bg"])
+    canvas.config(bg=selected_theme["bg"])
+    title_text.config(fg=selected_theme["work"], bg=selected_theme["bg"])
+    start_button.config(highlightbackground=selected_theme["bg"])
+    reset_button.config(highlightbackground=selected_theme["bg"])
+    check_mark.config(fg=selected_theme["work"], bg=selected_theme["bg"])
+    color_change.config(highlightbackground=selected_theme["bg"])
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
 def reset_timer():
     global timer
     global reps
@@ -20,27 +55,31 @@ def reset_timer():
     title_text.config(text="Timer")
     canvas.itemconfig(timer_text, text="00:00")
     window.after_cancel(timer)
-# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+# ---------------------------- TIMER MECHANISMS ------------------------------- #
 
 def start_timer():
     global reps
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
+    work_sec = 5
+    short_break_sec = 5
+    long_break_sec = 5
     reps += 1
 
     if reps % 8 == 0:
-        title_text.config(text="Long Break", fg=RED)
+        title_text.config(text="Long Break", fg=selected_theme["long"])
         count_down(long_break_sec)
     elif reps % 2 == 0:
-        title_text.config(text="Short Break", fg=PINK)
+        title_text.config(text="Short Break", fg=selected_theme["short"])
         count_down(short_break_sec)
     else:
-        title_text.config(text="Work, Bitch!", fg=GREEN)
+        title_text.config(text="Work, Bitch!", fg=selected_theme["work"])
         count_down(work_sec)
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def count_down(count):
 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec < 10:
@@ -54,35 +93,54 @@ def count_down(count):
         global reps
         checkmark = "✓"
         checks = ""
-        start_timer()
-        for n in range(math.floor(reps / 2)):
-            marks = checks + checkmark
-            check_mark.config(text=marks)
+        num_sessions = math.floor(reps / 2)
+        if num_sessions % 2 == 0:
+            print("hi")
+            start_timer()
+        else:
+            title_text.config(text="Press Start whenever\nyou're ready for another.", fg=selected_theme["long"])
+        for n in range(num_sessions):
+            checks += checkmark
+            check_mark.config(text=f"Sessions:\n {checks}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
+
+selected_theme = default
 window = Tk()
 window.title("My Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=selected_theme["bg"])
+# window.maxsize(width=650, height=450)
 
-title_text = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
-title_text.grid(column=1, row=0)
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# title_frame = tkinter.Frame(window, bg=selected_theme["bg"], height=10, width=50)
+title_text = Label(text="Timer", font=(FONT_NAME, 50), fg=selected_theme["work"], bg=selected_theme["bg"])
+title_text.grid(row=0, column=1, sticky=EW)
+
+# content_frame = tkinter.Frame(window)
+canvas = Canvas(width=200, height=224, bg=selected_theme["bg"], highlightthickness=0)
 tomato_pic = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_pic)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
-
-start_button = Button(text="Start", highlightbackground=YELLOW, command=start_timer)
+start_button = Button(text="Start", highlightbackground=selected_theme["bg"], command=start_timer)
 start_button.grid(column=0, row=2)
 
-
-reset_button = Button(text="Reset", highlightbackground=YELLOW, command=reset_timer)
+reset_button = Button(text="Reset", highlightbackground=selected_theme["bg"], command=reset_timer)
 reset_button.grid(column=2, row=2)
 
+check_mark = Label(text="Sessions:", fg=selected_theme["work"], bg=selected_theme["bg"], font=(FONT_NAME, 18, "bold"))
+check_mark.grid(column=0, row=4)
 
-check_mark = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 25, "bold"))
-check_mark.grid(column=1, row=3)
+color_change = Button(text="Color Theme", highlightbackground=selected_theme["bg"], command=change_colors)
+color_change.grid(column=2, row=4)
+
+# short_timer = Button(text="25min Timer", highlightbackground=selected_theme["bg"], command=partial(start_timer, short_timer_settings))
+# short_timer.grid(column=0, row=4)
+
+# long_timer = Button(text="50min Timer", highlightbackground=selected_theme["bg"], command=partial(start_timer, long_timer_settings))
+# long_timer.grid(column=1, row=4)
 
 window.mainloop()
